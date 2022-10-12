a333197f715d17e1f3316887d370037008918837
Danica Yener <ny21037@googlemail.com>
2022-10-12 15:52:35 +0200

    state-card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from "react"
+import CreditCard from "./CreditCard"
+
 
 function App() {
+const bankName = "Big Bank, Inc."
+const year ="08/19"
+const secCode = 1234
+const name = "DANICA YENER"
+const creditNumber ={
+  p1: 1234,
+  p2: 5678,
+  p3: 8765,
+  p4: 4321,
+}
+
+
   return (
     <div className="App">
-     <h1>My React App</h1>
+     <CreditCard
+      bankName= {bankName} expiryDate= {year} 
+      personName={name}
+      num={creditNumber} />
     </div>
   );
 }
